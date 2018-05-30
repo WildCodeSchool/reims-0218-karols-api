5b2dd0ebb2888ffbe4920c966eb0c55e9cdaf262
Cynnah <massot.dorian@gmail.com>
2018-05-30 11:53:52 +0200

    add a test path and test it

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+const test = {
+  name: "khalid",
+  say: "Hello"
+};
+
+router.get("/test", (req, res) => {
+  res.json(test);
 });
 
 module.exports = router;
