b673ce20ae3242158d432c8c59558cc1ea0151c6
Cynnah <massot.dorian@gmail.com>
2018-05-30 15:35:34 +0200

    add path prestations

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const prestations = require("../public/prestations.json");
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
@@ -13,6 +15,10 @@ const test = {
 
 router.get("/test", (req, res) => {
   res.json(test);
+});
+
+router.get("/prestations", (req, res) => {
+  res.json(prestations);
 });
 
 module.exports = router;
