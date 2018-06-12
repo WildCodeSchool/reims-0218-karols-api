cbb11cb12636c6c99af5cec8d5492261fcf8ad6f
Anahita <anahita.vahdani@neuf.fr>
2018-06-12 16:59:26 +0200

    delete boulette max again

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 const shopsPrestations = require("../public/shopsPrestations.json");
-const gendersServices = require("../public/gendersServices.json");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
