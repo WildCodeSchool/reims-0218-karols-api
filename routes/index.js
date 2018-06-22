202feae04b38d8a963f69ccded2543d28f658fe1
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-06-22 05:48:52 +0200

    NODEMAILEERRR POWAA

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,29 @@
-const express = require("express");
-const router = express.Router();
-const { DateTime } = require("luxon");
+const express = require("express")
+const router = express.Router()
+const { DateTime } = require("luxon")
 
-const shopsPrestations = require("../public/shopsPrestations.json");
-const createWeekTimeSlots = require("../timeslots/timeslots");
+const shopsPrestations = require("../public/shopsPrestations.json")
+const createWeekTimeSlots = require("../timeslots/timeslots")
+
+const nodemailer = require("nodemailer")
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
-});
+  res.render("index", { title: "Express" })
+})
 
 // POST method route
 router.post("/", function(req, res) {
-  res.send("POST request to the homepage");
-});
+  res.send("POST request to the homepage")
+})
 
 router.get("/test", (req, res) => {
-  res.json(test);
-});
+  res.json(test)
+})
 
 router.get("/shops-prestations", (req, res) => {
-  res.json(shopsPrestations);
-});
+  res.json(shopsPrestations)
+})
 
 router.get("/timeslots", (req, res) => {
   res.json(
@@ -32,12 +34,43 @@ router.get("/timeslots", (req, res) => {
         year: 2018
       })
     )
-  );
-});
+  )
+})
+
+router.post("/reservations", (req, res) => {
+  console.log("body", req.body)
+  let smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "marlowdevweb@gmail.com",
+      pass: "eRwKiiPwSpMr56wXCD"
+    }
+  })
+  console.log("coucou")
+  // parametre l'objet reservation ,
+  // module.export : sendMail
+
+  smtpTransport.sendMail(
+    {
+      from: "Valbuenaaaa <marlowdevweb@gmail.com>", // Expediteur
+      to: "marlot.tanguy@orange.fr", // Destinataires
+      subject: "bijour la famille Benzema en EDF !", // Sujet
+      text: "Coucou Zahia :coche_trait_plein:", // plaintext body
+      html: "<b>Hello worldd :coche_trait_plein:</b>" // html body
+    },
+    (error, response) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Message sent: Benzouille ")
+      }
+    }
+  )
+})
 
 router.get("/date-selected/:date", (req, res) => {
-  console.log("date : ", req.params.date);
-  res.send(createWeekTimeSlots(DateTime.fromISO(req.params.date)));
-});
+  console.log("date : ", req.params.date)
+  res.send(createWeekTimeSlots(DateTime.fromISO(req.params.date)))
+})
 
-module.exports = router;
+module.exports = router
--- routes/reservations.js
+++
@@ -1,4 +0,0 @@
-router.post("/reservations", function(req, res) {
-  console.log("body", req.body);
-  res.send("reservation faite");
-});
