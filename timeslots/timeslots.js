404a069b7d47cf376e1f9f7bdf5e89a5e2cf658b
Arno Wilder Reims <arnocrea@gmail.com>
2018-06-27 12:19:09 +0200

    first commit if condition

--- timeslots/timeslots.js
+++ timeslots/timeslots.js
@@ -1,4 +1,4 @@
-const { Interval, DateTime } = require("luxon")
+const { Interval, DateTime, Duration } = require("luxon")
 
 const startTime = DateTime.fromObject({
   // On definit l'heure de debut du timeSlot
@@ -36,19 +36,49 @@ const createWeekTimeSlots = date => {
   // On crée une fonction pour créer des timeslots par semaine de 5 jours
   // determiner la date du jour 1
   const dayArray = [] // declarer un tableau vide days
-  const day = date.minus({ days: 2 }) // On definit une constante day qui va debuté a jour actuel moins 2 jours
-  for (let i = 0; i < 5; i++) {
-    //  -> boucle qui passe 5 fois
-    dayArray.push({
-      // On pousse dans le tableau vide les timeSlots crées pour chaque jour
-      date: day.plus({ days: i }),
-      timeSlots: createDayTimeSlots(
-        day.plus({ days: i }).set({ hour: 9 }),
-        day.plus({ days: i }).set({ hour: 18 })
-      )
-    })
+  var today = new Date // Date du jour
+  var todayISO = DateTime.fromJSDate(today).toISODate() // Conversion en ISO de la date
+  var dateSelectedISO = DateTime.fromISO(date).toISODate()
+  console.log('date iso : ', todayISO)
+  console.log('date iso : ', dateSelectedISO)
+  console.log('equal', todayISO === dateSelectedISO)
+
+  const day = date.minus({ days: 0 }) // On definit une constante day qui va debuté a jour actuel moins 2 jours
+  if (todayISO === dateSelectedISO) {
+    console.log('la date est identique')
+    for (let i = 0; i < 5; i++) {
+      //  -> boucle qui passe 5 fois
+      dayArray.push({
+        // On pousse dans le tableau vide les timeSlots crées pour chaque jour
+        date: day.plus({ days: i }),
+        timeSlots: createDayTimeSlots(
+          day.plus({ days: i }).set({ hour: 9 }),
+          day.plus({ days: i }).set({ hour: 18 })
+        )
+      })
+    }
+    return dayArray
+  } else {
+    console.log('la date est différente')
+    const day = date.minus({ days: 2 }) // On definit une constante day qui va debuté a jour actuel moins 2 jours
+    for (let i = 0; i < 5; i++) {
+      //  -> boucle qui passe 5 fois
+      dayArray.push({
+        // On pousse dans le tableau vide les timeSlots crées pour chaque jour
+        date: day.plus({ days: i }),
+        timeSlots: createDayTimeSlots(
+          day.plus({ days: i }).set({ hour: 9 }),
+          day.plus({ days: i }).set({ hour: 18 })
+        )
+      })
+    }
+    return dayArray
+    
   }
-  return dayArray
+  
+  
+}
+
   // determiner la  day = date +i jour
   //startTime.plus({ days: i }), endTime.plus({ days: i })
   // ajouter à days l'objet suivant
@@ -58,6 +88,5 @@ const createWeekTimeSlots = date => {
       timeSlots: createDayTimeSlots(day 9h,  18h)
     }
   */
-}
 
 module.exports = createWeekTimeSlots
