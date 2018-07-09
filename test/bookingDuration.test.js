fc2c5ffcbc9f5ce136906112c0c2765840b443e0
tmt <thomas.culdaut@gmail.com>
2018-07-09 15:35:25 +0200

    fix intervalsfromduration

--- test/bookingDuration.test.js
+++ test/bookingDuration.test.js
@@ -1,6 +1,7 @@
 const { assert } = require("chai")
 const { Interval } = require("luxon")
 const createBookingDurations = require("../timeslots/createBookingDurations")
+const createBookingIntervalsFromDurations = require("../timeslots/createBookingIntervalsFromDurations")
 
 const exampleResources = [
   {
@@ -117,42 +118,6 @@ describe("createBookingDurations", () => {
     )
   })
 })
-
-const createBookingIntervalsFromDurations = (interval, bookingDurations) => {
-  let durationOfEach
-  let counter = 1
-  let arrayOfIntervals = []
-
-  for (booking of bookingDurations) {
-    if (counter <= 1) {
-      durationOfEach = Interval.after(interval.start, {
-        minutes: booking.duration.minutes
-      })
-      counter = counter + 1
-      const objectOfIntervals = {
-        name: booking.name,
-        type: booking.type,
-        interval: durationOfEach,
-        duration: booking.duration
-      }
-      arrayOfIntervals.push(objectOfIntervals)
-    } else {
-      const objectOfIntervals = {
-        name: booking.name,
-        type: booking.type,
-        interval: Interval.after(durationOfEach.end, {
-          minutes: booking.duration.minutes
-        }),
-        duration: booking.duration
-      }
-      durationOfEach = Interval.after(durationOfEach.end, {
-        minutes: booking.duration.minutes
-      })
-      arrayOfIntervals.push(objectOfIntervals)
-    }
-  }
-  return arrayOfIntervals
-}
 
 describe("createBookingIntervalsFromDurations", () => {
   it("should return an array of intervals + type ans name booking info", () => {
