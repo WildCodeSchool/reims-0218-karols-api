495adf652e21d634a25df504324b84222ac5f88c
Anahita <anahita.vahdani@neuf.fr>
2018-07-13 10:26:53 +0200

    change service description

--- migrations/3-create-services.js
+++ migrations/3-create-services.js
@@ -6,7 +6,7 @@ const allServices = [
     name: "Préparation",
     image:
       "https://images.unsplash.com/photo-1512496015851-a90fb38ba796?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e93589105d9189784b921b9b22be9826&auto=format&fit=crop&w=675&q=80",
-    description: "Venez vous faire maquiller",
+    description: "Venez vous faire préparer",
     selected: false
   },
   {
@@ -14,15 +14,15 @@ const allServices = [
     name: "Table",
     image:
       "https://images.unsplash.com/photo-1511914678378-2906b1f69dcf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0c0b02af4a4d6b94d894d454ef2d3e53&auto=format&fit=crop&w=634&q=80",
-    description: "Venez boire un coup",
+    description: "Réservez une table pour votre soirée",
     selected: false
   },
   {
     id: 3,
-    name: "Table + préparation",
+    name: "Table & Préparation",
     image:
       "https://images.unsplash.com/photo-1522936643032-5f3cde4cad06?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2a95c6a3a64dcf92424155361476dfc4&auto=format&fit=crop&w=1536&q=80",
-    description: "Venez vous faire maquiller et prendre un petit coup",
+    description: "Réservez votre table et vos préparations",
     selected: false
   }
 ]
